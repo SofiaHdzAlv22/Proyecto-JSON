40d5dec52deeeba2a24d7adf9e76df0dd6dd6752
SofiaHdzAlv22 <127440770+SofiaHdzAlv22@users.noreply.github.com>
2023-10-19 16:25:26 -0500

    Add files via upload

---
+++ JSON2 ShA =)/data/data.js
@@ -0,0 +1,33 @@
+export const data = 
+[
+    {
+        "id": 1,
+        "name": "Girasol",
+        "image": "/img/Girasol.jpg"
+    },
+    {
+        "id": 2,
+        "name": "Margaritas",
+        "image": "/img/Margaritas.jpg"  
+    },
+    {
+        "id": 3,
+        "name": "Orquídeas",
+        "image": "/img/Orquideas.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Peonias",
+        "image": "/img/Peonias.jpg"
+    },
+    {
+        "id": 5,
+        "name": "Rosas",
+        "image": "/img/Rosas.jpg"
+    },
+    {
+        "id": 6,
+        "name": "Tulipanes",
+        "image": "/img/Tulipanes.jpg"
+    }
+]
